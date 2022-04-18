5cbbbd0c144764ebfcde8de749021270acf2ea96
Nutthabhas Thitabhas <off1996@icloud.com>
2022-04-18 15:21:04 +0700

    add slideshow

--- script.js
+++ script.js
@@ -1,115 +1,136 @@
+// ----------------welcome------------------- //
+const welcome_message = document.getElementById('welcome-message');  
+const welcome = document.getElementById('welcome');
+const main = document.getElementById('main');
+const navbar = document.getElementById('top-nav');
+let sticky = navbar.offsetTop;
+console.log('position before main webpage show: '+sticky);
 
-window.onload = function () {
-    // ----------------welcome------------------- //
-    const welcome_message = document.getElementById('welcome-message');  
-    const welcome = document.getElementById('welcome');
-    const main = document.getElementById('main');
+//main function 
+//welcome fade-in
+// setTimeout(fadeIn,6000); // welcome to my website //
+// setTimeout(fadeOut,8000); //welcome fade-out and show main webpage
 
 
-    //main function 
-    //welcome fade-in
-    // setTimeout(fadeIn,6000); // welcome to my website //
+     //----FOR TEST----
+setTimeout(fadeIn,100);
+setTimeout(fadeOut,100);
 
-    //main function 
-    //welcome fade-out and show main webpage
-    // setTimeout(fadeOut,8000);
-
-    //----FOR TEST----
-    setTimeout(fadeIn,100);
-    setTimeout(fadeOut,100);
-
-    // tag p welcome function 
-    function fadeIn (){
-        setInterval(welcomeText,10); 
-    }
-    // welcome message
-    function welcomeText() {
+function fadeIn (){
+    let myInterval = setInterval(function() {
         let opacity = Number(window.getComputedStyle(welcome_message).getPropertyValue("opacity"));
-        if (opacity <= 1) {
-            opacity += 0.01;
-            welcome_message.style.opacity = opacity;
+            if (opacity < 1) {
+                opacity += 0.01;
+                welcome_message.style.opacity = opacity;
+                
+            } else {
+                clearInterval(myInterval);
         }
-        if (opacity >= 1) return;
-    } 
-
+    },10);
+}
     // welcome ani fadeout --> webpage
-    function fadeOut (){
-        setInterval(disr,10);
-    }
-    
 
-    // welcome disappear && show webpage
-    function disr(){
+function fadeOut (){
+    let myInterval = setInterval(function(){
         let opa = Number(window.getComputedStyle(welcome).getPropertyValue("opacity"));
-        if (opa > 0) {
-            opa -= 0.01;
-            welcome.style.opacity = opa; 
-            if (opa <= 0) {
+            if (opa > 0) {
+                opa -= 0.01;
+                welcome.style.opacity = opa; 
+            } else {
+                //-- these codes will execute when main webpage is loaded //
+
                 welcome.style.display = 'none';
                 main.style.display = 'block';
-                return;
-            }
-        }       
+                clearInterval(myInterval);
+                sticky = navbar.offsetTop;
+                console.log('position after main webpage show: '+sticky);
+                showSlides();
+    }},10);  
 }
+// welcome disappear && show webpage
 
     // ----------------------------change icon ------------------------------------//
-    const ig_icon = document.getElementById('ig-icon');
-    const fb_icon = document.getElementById('fb-icon');
-    const line_icon = document.getElementById('line-icon');
+const ig_icon = document.getElementById('ig-icon');
+const fb_icon = document.getElementById('fb-icon');
+const line_icon = document.getElementById('line-icon');
 
-    ig_icon.onmouseover = function () {
-        setTimeout(function () {
-            ig_icon.src = 'icon/ig_normal.png';
-        }, 100)
-    }
-    ig_icon.onmouseout = function () {
-        setTimeout(function () {
-            ig_icon.src = 'icon/ig_grey.png';   
-        }, 100)
-    }
-    fb_icon.onmouseover = function () {
-        setTimeout(function () {
-            fb_icon.src = 'icon/fb_normal.png';
-        }, 100)
-    }
-    fb_icon.onmouseout = function () {
-        setTimeout(function () {
-            fb_icon.src = 'icon/fb_grey.png';   
-        }, 100)
-    }
-    line_icon.onmouseover = function () {
-        setTimeout(function () {
-            line_icon.src = 'icon/line_normal.png';
-        }, 100)
-    }
-    line_icon.onmouseout = function () {
-        setTimeout(function () {
-            line_icon.src = 'icon/line_grey.png';   
-        }, 100)
-
-    }
-    //----------------------------- slide show ----------------------------- //
-    const slide = document.getElementsByClassName('myslide');
-    const dot = document.getElementsByClassName('dot');
-    let slideIndex = 0 ; 
-    showSlides();
-    
-    function showSlides() { 
-        let i;
-        for (i=0 ; i< slide.length ; i++) {
-            slide[i].style.display = "none";
-        }
-        for (i = 0; i < dot.length; i++) {
-            dot[i].className = dot[i].className.replace(" active", "");
-        }
-        slideIndex++ ;
-        console.log("slideIndex++ : "+slideIndex);
-
-        if (slideIndex > slide.length) {slideIndex=1} // set SlideIndex = 1
-        slide[slideIndex-1].style.display = "block";
-        dot[slideIndex-1].className += " active";
-        setTimeout(showSlides,2000);
-    }
-
-
+ig_icon.onmouseover = function () {
+    setTimeout(function () {
+        ig_icon.src = 'icon/ig_normal.png';
+    }, 100)
 }
+ig_icon.onmouseout = function () {
+    setTimeout(function () {
+        ig_icon.src = 'icon/ig_grey.png';   
+    }, 100)
+}
+fb_icon.onmouseover = function () {
+    setTimeout(function () {
+        fb_icon.src = 'icon/fb_normal.png';
+    }, 100)
+}
+fb_icon.onmouseout = function () {
+    setTimeout(function () {
+        fb_icon.src = 'icon/fb_grey.png';   
+    }, 100)
+}
+line_icon.onmouseover = function () {
+    setTimeout(function () {
+        line_icon.src = 'icon/line_normal.png';
+    }, 100)
+}
+line_icon.onmouseout = function () {
+    setTimeout(function () {
+        line_icon.src = 'icon/line_grey.png';   
+    }, 100)
+}
+//----------------------------- slide show ----------------------------- //
+const slide = document.getElementsByClassName('myslide');
+const dot = document.getElementsByClassName('dot');
+let slideIndex = 0 ;
+
+function showSlides(n) { 
+    let slideTimeout = setTimeout(showSlides,3000);
+    if (n != undefined) {
+        clearTimeout(slideTimeout);
+        slideIndex = n;
+    }
+    let i;
+    for (i=0 ; i< slide.length ; i++) {
+        slide[i].style.display = "none";
+    }
+    for (i = 0; i < dot.length; i++) {
+        dot[i].className = dot[i].className.replace(" active", "");
+    }
+    slideIndex++ ;
+    if (slideIndex > slide.length) {slideIndex=1} // set SlideIndex = 1
+    slide[slideIndex-1].style.display = "block";
+    dot[slideIndex-1].className += " active";
+    
+}
+function currentSlide(n){
+    showSlides(n);
+}
+//--- Hide top navbar on scroll ---//
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {  
+    myFunction()
+    let currentScrollpos = window.pageYOffset;
+    if (prevScrollpos > currentScrollpos) {
+        navbar.style.top ="0";
+    }else{
+        navbar.style.top ="-60px";
+    }
+    prevScrollpos = currentScrollpos;
+};  
+
+//--- add sticky class at top navbar ---//
+function myFunction(){
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+
