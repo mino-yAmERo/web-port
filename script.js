0e92dd4861809a34685035e9ce4e2d6504bec045
Nutthabhas Thitabhas <off1996@icloud.com>
2022-04-21 17:34:52 +0700

    add Codewars API card

--- script.js
+++ script.js
@@ -99,4 +99,53 @@ function myFunction(){
         navbar.classList.remove("sticky");
     }
 }
+window.onload= function () {
 
+const xhttp = new XMLHttpRequest();
+const url = 'https://www.codewars.com/api/v1/users/Mi No';
+xhttp.open('GET',url);
+xhttp.setRequestHeader('Content-Type','application/json');
+let codewars = document.getElementById('codewars-card');
+//-------- GET JSON --------//
+xhttp.onreadystatechange = function () {
+    if(this.readyState ==4 & this.status ==200) {
+        let myJSON = JSON.parse(this.responseText);
+        console.table(myJSON);
+        codewars.innerHTML = '<div>Username : ' + myJSON.username + '</div>';
+        codewars.innerHTML += '<div>Leaderboard : ' +myJSON.leaderboardPosition + '</div>';
+        codewars.innerHTML += '<div>Total Challenge Complete : ' + myJSON.codeChallenges.totalCompleted + '</div>';
+        codewars.innerHTML += '<div id="lang" onclick="showLang()" >Languages : '+ Object.keys(myJSON.ranks.languages).length+
+        '<button id="showLang"><span id="arrow" class="arrow-down"></button> </div>';
+
+
+        const Obj = myJSON.ranks.languages;
+        codewars.innerHTML += '<div id="langDetail" class="hide"></div>' ;
+        for (let x in Obj){
+            let text = "";
+            if(x.length <= 3) {
+                text += "<p><b>"+x.toUpperCase()+ "</b> : ";
+            } else {
+                text += "<p><b>"+x.charAt(0).toUpperCase()+x.slice(1)+ "</b> : ";
+            }
+            text += Obj[x]['name']+" | score : "+Obj[x]['score'];
+            document.getElementById('langDetail').innerHTML += text +"<br></p>";
+        }
+    }
+};
+
+
+xhttp.send();
+}
+let arrow = true;
+function showLang() {
+    arrow = !arrow;
+    if(arrow){
+        document.getElementById('arrow').classList.replace('arrow-up', 'arrow-down'); //replace arrow-down -> arrow up
+        document.getElementById('langDetail').classList.replace('show','hide'); // replace hide -> show
+    }else{
+        document.getElementById('arrow').classList.replace('arrow-down', 'arrow-up');
+        document.getElementById('langDetail').classList.replace('hide','show');
+    }
+};
+
+// https://www.codewars.com/api/v1/users/Mi%20No/code-challenges/completed?page=1
